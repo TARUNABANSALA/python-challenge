f25991ab7aa908f0c863303a20f2b611538c19d7
TARUNABANSALA <124708259+TARUNABANSALA@users.noreply.github.com>
2023-03-09 09:20:48 -0500

    Update poll.py

--- PyPoll/poll.py
+++ PyPoll/poll.py
@@ -4,11 +4,7 @@ read_fpath = ("C:/Users/panka/OneDrive/Desktop/Class Folder/Github/python-challe
 Votes = []
 Candidates = []
 County = []
-Candidate1 = 0
-Candidate2 = 0
-Candidate3 = 0
 Net_total_Votes = 0
-count1 = 0
 Candidates_dictionary = {}
 with open(read_fpath, 'r') as f: 
     reader = csv.reader(f, delimiter=',')
@@ -21,14 +17,17 @@ with open(read_fpath, 'r') as f:
     for x in range(len(Votes)):
         Net_total_Votes = Net_total_Votes + 1
 # #  A complete list of candidates who received votes
-    Names = list(dict.fromkeys(Candidates))
-      # * The total number of votes cast
-    Candidates_dictionary = dict(zip(County, Candidates))
-print(Candidates_dictionary)               
-print(f"Total Net Votes = {Net_total_Votes}")
-print(Names[0])
-print(Names[1])
-print(Names[2])
+    candidatesSet = set(Candidates)
+    for items in candidatesSet:
+        count = Candidates.count(items)  
+        print(f"{items} appears {count} times") 
+# Names = list(dict.fromkeys(Candidates))
+#       # * The total number of votes cast
+
+# print(f"Total Net Votes = {Net_total_Votes}")
+# print(Names[0])
+# print(Names[1])
+# print(Names[2])
 # * The percentage of votes each candidate won = "particular votes/ total votes"
 # * The total number of votes each candidate won = total particular votes
 # * The winner of the election based on popular vote.
