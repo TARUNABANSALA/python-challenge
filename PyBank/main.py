ef5d56c9e53e7fae8042bf9ae2a6e29c16127cb5
TARUNABANSALA <124708259+TARUNABANSALA@users.noreply.github.com>
2023-03-13 10:24:36 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,15 @@
 import csv
 read_fpath = ("PyBank/Resources/budget_data.csv")
-# Assigning variables
-change_profit_losses_list = [] 
+
+# Assigning variables: all the variables & lists & dictionaries have been defined thoroughly in the readme file
+Profitlossesdate = {}
 netAmount = 0
+change_profit_losses_list = []
+changeProfitLossDict = {} 
 sum_of_change = 0
-dictionary = {}
+greatest_increase_profits_date = ''
+greatest_decrease_profits_date = ''
+
 #Reading the file through csv reader
 with open(read_fpath, 'r') as f: 
     reader = csv.reader(f, delimiter=',')
@@ -12,23 +17,18 @@ with open(read_fpath, 'r') as f:
     for row in reader: 
 # Remove the header by putting an IF statement
         if(row[1] != 'Profit/losses'):
-            dictionary[row[1]]= row[0]
+            Profitlossesdate[row[1]]= row[0]
 # Calculations: Calculating Total Net Amount
             netAmount = netAmount + int(row[1])
 # Calculating Total Months
-    totalMonths = len(dictionary.values())
-# Calculating Average Change
-    # Making a dictionary for change in profit/losses
-    changeProfitLossDict = {}
-    greatest_increase_profits_date = ''
-    greatest_decrease_profits_date = ''
-    # Making a list of Profit/losses using dictionary key values
-    profitLossList = list(dictionary.keys())
+    totalMonths = len(Profitlossesdate.values())
+# Calculating Average Change: # Making a dictionary for change in profit/losses + # Making a list of Profit/losses using dictionary key values
+    profitLossList = list(Profitlossesdate.keys())
     for i in range(0, len(profitLossList)-1):
         changeInProfitLoss = int(profitLossList[i+1]) - int(profitLossList[i])
         change_profit_losses_list.append(changeInProfitLoss)
         # getting the date of Change by calling values from dictionary
-        dateOfChange = dictionary.get(profitLossList[i+1])
+        dateOfChange = Profitlossesdate.get(profitLossList[i+1])
         # Making a new dictionary of key: changeInProfitLoss and value: dateOfChange
         changeProfitLossDict[changeInProfitLoss] = dateOfChange
         sum_of_change = (sum_of_change + int(change_profit_losses_list[i]))
@@ -39,6 +39,8 @@ with open(read_fpath, 'r') as f:
     greatest_increase_profits_date = changeProfitLossDict.get(greatest_increase_profits)
     greatest_decrease_profits = min(change_profit_losses_list)
     greatest_decrease_profits_date = changeProfitLossDict.get(greatest_decrease_profits)
+
+# Printing the results according to the given format
 print("```text")
 print("Financial Analysis")
 print("----------------------------")
@@ -48,6 +50,7 @@ print(f"Average change = {Average_change}")
 print(f"Greatest Increase in Profits = {greatest_increase_profits_date} {greatest_increase_profits}")
 print(f"Greatest Decrease in Profits = {greatest_decrease_profits_date} {greatest_decrease_profits}")
 print("```")
+# Exported a text file with the results in file named "BankOutput.txt".
 # Specify the file path
 file_path = 'PyBank/Analysis/BankOutput.txt'
 # Open the file for writing
